5edcf5750fa3b1c78f598f45ab2e36791116465e
JhonnSteven23 <160976036+JhonnSteven23@users.noreply.github.com>
2025-11-11 22:33:48 -0400

    [Fix] Arreglos para el error de text

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,14 +1,23 @@
-import { View, type ViewProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/use-theme-color';
+import React from 'react';
+import { Text, View, type ViewProps } from 'react-native';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({ style, lightColor, darkColor, children, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  const safeChildren = React.Children.map(children, (child, index) => {
+    if (typeof child === 'string' || typeof child === 'number') {
+      return (
+        <Text key={String(index)}>{child}</Text>
+      );
+    }
+    return child;
+  });
+
+  return <View style={[{ backgroundColor }, style]} {...otherProps}>{safeChildren}</View>;
 }
